54296d91cb92f2ac6a06854737d2c841916cee7d
Jerome Wu <jeromewus@gmail.com>
2015-03-05 01:30:52 +0800

    V1.0

--- makefile
+++ makefile
@@ -1,8 +1,9 @@
 name=swrlapp
 main=com.delta.SWRLApp
 jarfile=./target/${name}-1.0-SNAPSHOT-jar-with-dependencies.jar
-ontfile=./data/ont/demoData.rdf
-rulefile=./data/rule/demo.rule
+ontfile=./data/ont/meal.rdf
+rulefile=./data/rule/meal.rule
+jsonfile=./data/json/in1.json
 log4jproperties=./src/main/resources/log4j.properties
 
 all: compile
@@ -11,4 +12,4 @@ compile:
 	mvn clean compile assembly:single
 
 run:
-	java -cp ${jarfile} -Dlog4j.configuration=file:${log4jproperties} ${main} ${ontfile} ${rulefile}
+	java -cp ${jarfile} -Dlog4j.configuration=file:${log4jproperties} ${main} ${ontfile} ${rulefile} ${jsonfile}
--- src/main/java/com/delta/SWRLApp.java
+++ src/main/java/com/delta/SWRLApp.java
@@ -1,12 +1,6 @@
 package com.delta;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.File;
+import java.util.Iterator;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -23,64 +17,85 @@ import com.hp.hpl.jena.util.FileManager;
 
 import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasonerFactory;
 import com.hp.hpl.jena.reasoner.rulesys.Rule;
+import com.hp.hpl.jena.vocabulary.ReasonerVocabulary;
+
+import com.hp.hpl.jena.util.PrintUtil;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class SWRLApp 
 {
+    final static String mealURI = "http://www.delta.com.tw/meal#";
+    final static String wineURI = "http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#";
+    final static String foodURI = "http://www.w3.org/TR/2003/PR-owl-guide-20031209/food#";
+    final static String rdfsURI = "http://www.w3.org/2000/01/rdf-schema#";
+
     public static void main( String[] args )
     {
-      String RDFFilePath = args[0];
-      String RuleFilePath = args[1];
+      String rdfFilePath = args[0];
+      String ruleFilePath = args[1];
+      String JSONFilePath = args[2];
 
-      Model model = getModel(RDFFilePath);
-      String rules = readFile(RuleFilePath, Charset.defaultCharset());
-      Reasoner reasoner = getReasoner(rules);
-      InfModel inf = ModelFactory.createInfModel(reasoner, model);
+      Model model = getModel(rdfFilePath);
+      Resource target = createTargetResource(model, JSONFilePath);
+      Reasoner reasoner = getReasoner(ruleFilePath);
+      InfModel infModel = ModelFactory.createInfModel(reasoner, model);
       
-      printAllStatement(inf);
-      System.out.println(rules);
-      System.out.println(getTimeStamp());
-    }
-
-    private static String getTimeStamp() {
-      return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS+08:00").format(new Date());
+      registerPrefix();
+      //printAllStatement(model);
+      //printAllStatement(inf);
+      Property subClassOf = infModel.getProperty(rdfsURI, "subClassOf");
+      target = infModel.getResource(mealURI+"target");
+      System.out.println(target.getProperty(subClassOf).getObject());
     }
 
     private static Model getModel(String filePath) {
       return FileManager.get().loadModel("file:"+filePath);
     }
 
-    private static Reasoner getReasoner(String rules) {
-      return new GenericRuleReasoner(Rule.parseRules(rules));
-    }
-
-    private static String readFile(String filePath, Charset encoding) {
-      String ret = new String();
-      try {
-        byte[] encoded = Files.readAllBytes(Paths.get(filePath));
-        ret = new String(encoded, encoding);
-      } catch(IOException e) {
-        ;
-      } 
-      return ret;
+    private static Reasoner getReasoner(String ruleFilePath) {
+      Model model = ModelFactory.createDefaultModel();
+      Resource config = model.createResource();
+      config.addProperty(ReasonerVocabulary.PROPruleMode, "hybrid");
+      config.addProperty(ReasonerVocabulary.PROPruleSet, ruleFilePath);
+      return GenericRuleReasonerFactory.theInstance().create(config);
     }
 
     private static void printAllStatement(Model model) {
       StmtIterator iter = model.listStatements();
       while(iter.hasNext()) {
-        Statement stmt = iter.nextStatement();
-        Resource subject = stmt.getSubject();
-        Property predicate = stmt.getPredicate();
-        RDFNode object = stmt.getObject();
-
-        System.out.print(subject.toString());
-        System.out.print(" " + predicate.toString() + " ");
-        if(object instanceof Resource) {
-          System.out.print(object.toString());
-        } else {
-          System.out.print("\"" + object.toString() + "\"");
-        }
-        System.out.println(" .");
+        System.out.println(PrintUtil.print(iter.nextStatement()));
       }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Resource createTargetResource(Model model, String JSONFilePath) {
+      Resource target = model.createResource(mealURI+"target");
+      JSONParser parser = new JSONParser();
+      try {
+        Object obj = parser.parse(new FileReader(JSONFilePath));
+        JSONObject jsonObject = (JSONObject)obj;
+        JSONArray input = (JSONArray)jsonObject.get("input");
+        Iterator<JSONObject> iterator = input.iterator();
+        while(iterator.hasNext()) {
+          JSONObject t = iterator.next();
+          Property p = model.getProperty(mealURI, (String)t.get("property"));
+          Resource r = model.getResource((String)t.get("value"));
+          target.addProperty(p, r);
+        }
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+      return target;
+    }
+
+    private static void registerPrefix() {
+      PrintUtil.registerPrefix("food", foodURI);
+      PrintUtil.registerPrefix("wine", wineURI);
+      PrintUtil.registerPrefix("meal", mealURI);
     }
 }
