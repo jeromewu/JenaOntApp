4f39012c2e33ede2eb284016af510e301bf8ab0a
Jerome Wu <jeromewus@gmail.com>
2015-03-05 01:46:49 +0000

    get prefix from rule file

--- src/main/java/com/delta/SWRLApp.java
+++ src/main/java/com/delta/SWRLApp.java
@@ -1,7 +1,12 @@
 package com.delta;
 
 import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -29,27 +34,25 @@ import org.json.simple.parser.JSONParser;
 
 public class SWRLApp 
 {
-    final static String mealURI = "http://www.delta.com.tw/meal#";
-    final static String wineURI = "http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#";
-    final static String foodURI = "http://www.w3.org/TR/2003/PR-owl-guide-20031209/food#";
-    final static String rdfsURI = "http://www.w3.org/2000/01/rdf-schema#";
+    final static String targetLocalName = "target";
+    final static String targetNameSpace = "http://www.delta.com.tw/runtime#";
 
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
       String rdfFilePath = args[0];
       String ruleFilePath = args[1];
       String JSONFilePath = args[2];
 
+      Map<String, String> prefixMap = getPrefixMap(ruleFilePath);
+      registerPrefix(prefixMap);
+
       Model model = getModel(rdfFilePath);
-      Resource target = createTargetResource(model, JSONFilePath);
+      Resource target = createTargetResource(model, JSONFilePath, prefixMap);
       Reasoner reasoner = getReasoner(ruleFilePath);
       InfModel infModel = ModelFactory.createInfModel(reasoner, model);
       
-      registerPrefix();
-      //printAllStatement(model);
-      //printAllStatement(inf);
-      Property subClassOf = infModel.getProperty(rdfsURI, "subClassOf");
-      target = infModel.getResource(mealURI+"target");
+      Property subClassOf = infModel.getProperty(prefixMap.get("rdfs"), "subClassOf");
+      target = infModel.getResource(getURI(targetNameSpace, targetLocalName));
       System.out.println(target.getProperty(subClassOf).getObject());
     }
 
@@ -73,29 +76,66 @@ public class SWRLApp
     }
 
     @SuppressWarnings("unchecked")
-    private static Resource createTargetResource(Model model, String JSONFilePath) {
-      Resource target = model.createResource(mealURI+"target");
-      JSONParser parser = new JSONParser();
-      try {
-        Object obj = parser.parse(new FileReader(JSONFilePath));
-        JSONObject jsonObject = (JSONObject)obj;
-        JSONArray input = (JSONArray)jsonObject.get("input");
-        Iterator<JSONObject> iterator = input.iterator();
-        while(iterator.hasNext()) {
-          JSONObject t = iterator.next();
-          Property p = model.getProperty(mealURI, (String)t.get("property"));
-          Resource r = model.getResource((String)t.get("value"));
+    private static Resource createTargetResource(Model model, String JSONFilePath, Map<String, String> prefixMap) throws Exception{
+      Resource target = model.createResource(getURI(targetNameSpace, targetLocalName));
+      JSONObject jsonObject = parseJSON(JSONFilePath);
+
+      JSONArray args = (JSONArray)jsonObject.get("args");
+      Iterator<JSONObject> iterator = args.iterator();
+      while(iterator.hasNext()) {
+        JSONObject obj = iterator.next();
+
+        String propertyNS = (String)obj.get("propertyNS");
+        String property = (String)obj.get("property");
+        String valueNS = (String)obj.get("valueNS");
+        String value = (String)obj.get("value");
+
+        Property p = model.getProperty(prefixMap.get(propertyNS), property);
+        if(valueNS != "xsd") {
+          Resource r = model.getResource(getURI(prefixMap.get(valueNS), value));
           target.addProperty(p, r);
+        } else {
+          ; 
         }
-      } catch(Exception e) {
-        e.printStackTrace();
       }
+
       return target;
     }
 
-    private static void registerPrefix() {
-      PrintUtil.registerPrefix("food", foodURI);
-      PrintUtil.registerPrefix("wine", wineURI);
-      PrintUtil.registerPrefix("meal", mealURI);
+    private static JSONObject parseJSON(String JSONFilePath) throws Exception {
+      JSONParser parser = new JSONParser();
+      return (JSONObject)parser.parse(new FileReader(JSONFilePath));
+    }
+
+    private static void registerPrefix(Map<String, String> prefixMap) {
+      for(String key : prefixMap.keySet()) {
+        PrintUtil.registerPrefix(key, prefixMap.get(key));
+      }
+    }
+
+    private static String getURI(String nameSpace, String localName) {
+      return nameSpace + localName;
+    }
+
+    private static Map<String, String> getPrefixMap(String ruleFilePath) throws Exception {
+      Map<String, String> prefixMap = new HashMap<String, String>();
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(ruleFilePath));
+      String line = new String("");
+      while((line = bufferedReader.readLine()) != null) {
+        if(line.startsWith("@prefix")) {
+          String[] seg = line.split(" ");
+          String prefix = seg[1].substring(0, seg[1].length()-1);
+          String uri = extractString(seg[2], ".*\\< *(.*) *\\>*.");
+          prefixMap.put(prefix, uri);
+        }
+      }
+      return prefixMap;
+    }
+
+    private static String extractString(String input, String regexp) {
+      Pattern pattern = Pattern.compile(regexp);
+      Matcher matcher = pattern.matcher(input);
+      matcher.find();
+      return matcher.group(1);
     }
 }
